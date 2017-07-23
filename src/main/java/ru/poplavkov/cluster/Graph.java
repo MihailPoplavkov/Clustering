3dd3a8a288cbe9b2bc602f6a4d2dfdcfbca2e810
Mihail Poplavkov <mihail.poplavkov@gmail.com>
2017-07-23 15:17:41 +0300

    Added .writeOnDisk() method to Graph

--- src/main/java/ru/poplavkov/cluster/Graph.java
+++ src/main/java/ru/poplavkov/cluster/Graph.java
@@ -2,17 +2,26 @@ package ru.poplavkov.cluster;
 
 import lombok.Getter;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
 
 @SuppressWarnings("WeakerAccess")
 @Getter
 public class Graph {
+    private int counter;
     private Set<String> queries = new HashSet<>();
     private Set<String> documents = new HashSet<>();
     private Map<Tuple, Integer> links = new HashMap<>();
+
+    public Graph(int counter) {
+        this.counter = counter;
+    }
+
+    public Graph() {
+        this(0);
+    }
 
     public void addLink(String query, String document) {
         queries.add(query);
@@ -22,4 +31,65 @@ public class Graph {
         count = count == null ? 1 : count + 1;
         links.put(tuple, count);
     }
+
+    public boolean writeOnDisk(String path) {
+        if (path.charAt(path.length() - 1) == '/') {
+            path = path.substring(0, path.length() - 1);
+        }
+        counter++;
+        File queriesFile = createFile(path, ".queries", counter);
+        File documentsFile = createFile(path, ".documents", counter);
+        File linksFile = createFile(path, ".links", counter);
+        return writeOnDisk(queriesFile, queries) &&
+                writeOnDisk(documentsFile, documents) &&
+                writeOnDisk(linksFile, links);
+    }
+
+    private boolean writeOnDisk(File file, Set<String> set) {
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            set.stream()
+                    .sorted()
+                    .forEach(s -> {
+                        try {
+                            fos.write(String.format("%s%n", s).getBytes());
+                        } catch (IOException e) {
+                            // TODO: написать handler
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean writeOnDisk(File file, Map<Tuple, Integer> map) {
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            map.entrySet()
+                    .stream()
+                    .sorted(Comparator.comparing(Map.Entry::getKey))
+                    .forEach(es -> {
+                        try {
+                            Tuple tuple = es.getKey();
+                            fos.write(
+                                    String.format("%s:%s=%d%n",
+                                            tuple.getQuery(), tuple.getDocument(), es.getValue())
+                                            .getBytes());
+                        } catch (IOException e) {
+                            // TODO: написать handler
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private File createFile(String path, String name, int counter) {
+        return new File(String.format("%s/%s%d", path, name, counter));
+    }
+
 }
--- src/test/java/ru/poplavkov/cluster/GraphTest.java
+++ src/test/java/ru/poplavkov/cluster/GraphTest.java
@@ -1,27 +1,38 @@
 package ru.poplavkov.cluster;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GraphTest {
-    @Test
-    void addLink() {
-        Graph graph = new Graph();
-        String q1 = "ab";
-        String q2 = "cd";
-        String d1 = "ef";
-        String d2 = "gh";
+    private static Graph graph;
+    private static String q2 = "ab";
+    private static String q1 = "cd";
+    private static String d1 = "ef";
+    private static String d2 = "gh";
 
+    @BeforeAll
+    static void init() {
+        graph = new Graph();
         graph.addLink(q1, d1);
         graph.addLink(q1, d2);
         graph.addLink(q1, d2);
         graph.addLink(q1, d2);
         graph.addLink(q2, d2);
+    }
 
+    @Test
+    void addLink() {
         Set<String> queries = graph.getQueries();
         Set<String> documents = graph.getDocuments();
 
@@ -29,5 +40,58 @@ class GraphTest {
         assertTrue(documents.containsAll(Arrays.asList(d1, d2)));
 
         assertEquals(3, graph.getLinks().get(new Tuple(q1, d2)).intValue());
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    void writeOnDisk() {
+        String path = "src/test/resources";
+        assertTrue(graph.writeOnDisk(path));
+
+        File queriesFile = new File(String.format("%s/%s", path, ".queries1"));
+        File documentsFile = new File(String.format("%s/%s", path, ".documents1"));
+        File linksFile = new File(String.format("%s/%s", path, ".links1"));
+
+        assertTrue(assertExist(queriesFile, graph.getQueries()));
+        assertTrue(assertExist(documentsFile, graph.getDocuments()));
+        assertTrue(assertExist(linksFile, graph.getLinks()));
+
+        queriesFile.delete();
+        documentsFile.delete();
+        linksFile.delete();
+    }
+
+    private boolean assertExist(File file, Set<String> set) {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = br.readLine()) != null) {
+                assertTrue(set.remove(line));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean assertExist(File file, Map<Tuple, Integer> map) {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String tokens[] = line.split(":", 2);
+                String q = tokens[0];
+                tokens = tokens[1].split("=", 2);
+                String d = tokens[0];
+                Integer i = Integer.valueOf(tokens[1]);
+                Tuple tuple = new Tuple(q, d);
+                assertEquals(map.remove(tuple), i);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
